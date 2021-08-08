34cf9dd358c3f5c0c0fcae45f1646b2329cfa677
julioclopes32 <julioclopes32@gmail.com>
2021-08-08 04:27:19 -0300

    Register front-End Finished

--- app/src/main/java/com/example/vollup_app_android/Login.java
+++ app/src/main/java/com/example/vollup_app_android/Login.java
@@ -2,13 +2,33 @@ package com.example.vollup_app_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    TextView register;
+    private long mLastClickTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = findViewById(R.id.register);
+        
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (SystemClock.elapsedRealtime() - mLastClickTime < 1000){
+                    return;
+                }
+                mLastClickTime = SystemClock.elapsedRealtime();
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
     }
 }
